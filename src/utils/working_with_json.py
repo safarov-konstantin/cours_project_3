12b3b1a305adb382ff88069c2d1e5d4f3f141f29
Konstantin Safarov <safarov-konstantin@yandex.ru>
2023-09-25 10:24:15 +0300

    Добавляет функционал работы с данными
     изменено:      requirements.txt
    	дабавляет зависимость пактета "datetime"
    
     новый файл:    src/utils/working_with_json.py
    	дабавляет функцию load_json()
    	дабавляет функцию select_executed_operation()
    	дабавляет функцию sort_descending()
    
     новый файл:    tests/test_working_with_json.py
    	добавляет тесты функции test_load_json
    	добавляет тесты функции test_select_executed_operation
    	добавляет тесты функции test_sort_descending

---
+++ src/utils/working_with_json.py
@@ -0,0 +1,36 @@
+from json import loads
+from datetime import datetime
+
+
+def load_json(path: str):
+    """
+    Читает файл с данными в формате json
+    и дессерилизует данные json
+    :param path: путь данным файла в формате json
+    :return: дессериализованную струкутуру
+    """
+    with open(path, encoding='utf8') as file:
+        return loads(file.read())
+
+
+def select_executed_operation(operations):
+    """
+    Возвразащает list выполненных банковских операций
+    :param operations: list банковских операций
+    :return: list выполенных операций
+    """
+    return [operation for operation in operations if operation["state"] == 'EXECUTED']
+
+
+def sort_descending(operations):
+    """
+    Сортивка банковских операций по убыванию
+    :param operations: list банковских операций
+    :return: list отсортированный по убыванию  банковсих операций
+    """
+    sort_operations = sorted(
+        operations,
+        key=lambda operation: datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+        reverse=True
+    )
+    return sort_operations
